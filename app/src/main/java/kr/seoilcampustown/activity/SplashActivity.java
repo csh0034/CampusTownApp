f8e2523bb3f272f52e8b7f9ed854576207dd5e53
csh <csh0034@gmail.com>
2019-10-17 16:02:46 +0900

    테마이용 스플래시 추가

---
+++ app/src/main/java/kr/seoilcampustown/activity/SplashActivity.java
@@ -0,0 +1,28 @@
+package kr.seoilcampustown.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(() -> {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }, 1000);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+}
